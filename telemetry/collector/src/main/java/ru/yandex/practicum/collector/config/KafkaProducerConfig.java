856a6e354829372e4b84caec8950e1b09f581510
neryklin <ryklin@yandex.ru>
2026-03-16 20:07:37 +0300

    16.03.26 исправлены замечания код ревью

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaProducerConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaProducerConfig.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.collector.config;
 
 import org.apache.avro.specific.SpecificRecordBase;
-import ru.practicum.kafka.serializer.AvroSerializer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import ru.practicum.kafka.serializer.AvroSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
