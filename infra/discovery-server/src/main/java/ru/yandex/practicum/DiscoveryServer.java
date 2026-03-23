8614d45a5b74b4456d01eabb07c9a93bc0d648a8
neryklin <ryklin@yandex.ru>
2026-03-24 01:01:19 +0300

    24,03,26

--- infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
@@ -1,4 +1,4 @@
-package practicum;
+package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryServer {
     public static void main(String[] args) {
+
         SpringApplication.run(DiscoveryServer.class, args);
     }
 }
